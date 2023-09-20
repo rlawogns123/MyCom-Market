407136c109a6a277971e3389173f24cba4df4e85
rmawogns <evolui@naver.com>
2023-09-20 23:52:35 +0900

    test3

--- App/index.js
+++ App/index.js
@@ -6,32 +6,26 @@ const app = express();
 const port = 4000;
 const config = require("./server/config/key.js");
 
-app.get("/hello", (req, res) => {
-  res.status(200).json({ message: "hello" });
-});
-
-app.listen(port);
-
 // app.use(express.static(path.join(__dirname, "./client/build")));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/product", require("./server/router/product.js"));
-// app.use("/api/user", require("./server/router/user.js"));
-// app.use("/api/reple", require("./server/router/reple.js"));
-// app.use("/image", express.static("./image"));
+app.use("/api/product", require("./server/router/product.js"));
+app.use("/api/user", require("./server/router/user.js"));
+app.use("/api/reple", require("./server/router/reple.js"));
+app.use("/image", express.static("./image"));
 
-// app.listen(port, () => {
-//   mongoose
-//     .connect(config.mongoURI)
-//     .then(() => {
-//       console.log(`Example app listening on port ${port}`);
-//       console.log("Connecting MongoDB...");
-//     })
-//     .catch((err) => {
-//       console.log(`${err}`);
-//     });
-// });
+app.listen(port, () => {
+  mongoose
+    .connect(config.mongoURI)
+    .then(() => {
+      console.log(`Example app listening on port ${port}`);
+      console.log("Connecting MongoDB...");
+    })
+    .catch((err) => {
+      console.log(`${err}`);
+    });
+});
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
