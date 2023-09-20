046edc50d423ea430b9d1fc13bbcaddb61e3ee5f
rmawogns <evolui@naver.com>
2023-09-20 23:54:50 +0900

    test4

--- App/index.js
+++ App/index.js
@@ -15,17 +15,7 @@ app.use("/api/user", require("./server/router/user.js"));
 app.use("/api/reple", require("./server/router/reple.js"));
 app.use("/image", express.static("./image"));
 
-app.listen(port, () => {
-  mongoose
-    .connect(config.mongoURI)
-    .then(() => {
-      console.log(`Example app listening on port ${port}`);
-      console.log("Connecting MongoDB...");
-    })
-    .catch((err) => {
-      console.log(`${err}`);
-    });
-});
+app.listen(port);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
